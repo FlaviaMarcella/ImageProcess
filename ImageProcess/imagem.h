386fe69463184b9b44590b4a7b02b65d281e5658
FlaviaMarcella <flaviamgm011@gmail.com>
2024-06-24 19:25:42 -0300

    create type Matriz

---
+++ ImageProcess/imagem.h
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   imagem.h
+ * Author: fl-via-gon-alves
+ *
+ * Created on 24 de junho de 2024, 18:23
+ */
+
+#ifndef IMAGEM_H
+#define IMAGEM_H
+
+typedef int TMatriz[1024][1024]; 
+
+
+#endif /* IMAGEM_H */
+
